43aeabe5874d666c093a21d7df95040496a0610c
Adrian-Utah <u1350058@utah.edu>
2026-02-02 08:14:42 -0700

    switch blinking to other lights

--- lab1/Src/lab1.c
+++ lab1/Src/lab1.c
@@ -13,12 +13,18 @@ HAL_Init();
 SystemClock_Config();
 
 __HAL_RCC_GPIOC_CLK_ENABLE();
+/*
 GPIO_InitTypeDef initStr = {GPIO_PIN_8 | GPIO_PIN_9, GPIO_MODE_OUTPUT_PP, GPIO_SPEED_FREQ_LOW,GPIO_NOPULL};
 HAL_GPIO_Init(GPIOC, &initStr);
 HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8, GPIO_PIN_SET);
+*/
+GPIO_InitTypeDef initStr = {GPIO_PIN_6 | GPIO_PIN_7, GPIO_MODE_OUTPUT_PP, GPIO_SPEED_FREQ_LOW,GPIO_NOPULL};
+HAL_GPIO_Init(GPIOC, &initStr);
+HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6, GPIO_PIN_SET);
   while (1)
   {
     HAL_Delay(200);
+    //HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_8|GPIO_PIN_9);
     HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_8|GPIO_PIN_9);
   }
   return -1;
